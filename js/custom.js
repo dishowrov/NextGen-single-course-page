98e9561b25dd96aadb327781905b4f402f57394b
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-08 17:09:55 +0600

    done

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,21 @@
-$("#dis-ng-slick-slider").slick({
+$(".dis-ng-slick-slider").slick({
   infinite: true,
   autoplay: true,
   autoplaySpeed: 1357,
+  centerPadding: '12px',
   slidesToShow: 4,
   slidesToScroll: 1,
-  prevArrow: '<button type="button" class="slick-prev dis-ng-slick-slider-arrow dis-ng-slick-left-arrow"> <img src="../assets/slider-left-arrow.png"> </button>',
-  nextArrow: '<button type="button" class="slick-prev dis-ng-slick-slider-arrow dis-ng-slick-right-arrow"> <img src="../assets/slider-right-arrow.png"> </button>',
+  prevArrow: '<button type="button" class="slick-prev dis-ng-slick-slider-arrow dis-ng-slick-left-arrow"> <img src="./assets/slider-left-arrow.png"> </button>',
+  nextArrow: '<button type="button" class="slick-prev dis-ng-slick-slider-arrow dis-ng-slick-right-arrow"> <img src="./assets/slider-right-arrow.png"> </button>',
   responsive: [   
     { 
-      breakpoint: 1024,  
+      breakpoint: 1165,  
       settings: {
         slidesToShow: 3,
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 576,
       settings: {
         slidesToShow: 2,
       },

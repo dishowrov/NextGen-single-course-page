04ea871f4ad1aae643e705fb66efeecca27dddcf
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-19 16:29:05 +0600

    more

--- owl-slider/assets/js/custom.js
+++ owl-slider/assets/js/custom.js
@@ -1,12 +1,39 @@
+// $(function () {
+//     var owl = $(".dis-carousel");
+//     owl.owlCarousel({ 
+//         items: 4,
+//         margin: 15,
+//         loop: true,
+//         navText: ['<i class="fas fa-chevron-left"></i>' , '<i class="fas fa-chevron-right"></i>'],
+//         nav: true,
+//         dots: false,
+//         responsive: {
+//             480: {
+//                 items: 2,
+//             },
+//             768: {
+//                 items: 3,
+//             },
+//             992: {
+//                 items: 4,
+//             }
+//         },
+//     });
+// });
+
 $(function () {
-    var owl = $(".dis-carousel");
+    // Owl Carousel
+    var owl = $(".owl-carousel");
     owl.owlCarousel({
         items: 4,
-        margin: 15,
+        margin: 10,
         loop: true,
-        navText: [$('.am-prev'), $('.am-next')],
         nav: true,
         dots: false,
+        navText: [
+            "<i aria-hidden='true' class='fas fa-chevron-left'></i>",
+            "<i aria-hidden='true' class='fas fa-chevron-right'></i>",
+        ],
         responsive: {
             480: {
                 items: 2,
@@ -19,4 +46,4 @@ $(function () {
             }
         },
     });
-});
+}); 

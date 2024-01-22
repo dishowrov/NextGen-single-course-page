df81a36847ee8e4022069e86a5fa9391876531ae
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-22 11:19:56 +0600

    done

--- owl-slider/assets/js/custom.js
+++ owl-slider/assets/js/custom.js
@@ -1,22 +1,22 @@
 $(function () {
     var owl = $(".dis-carousel");
     owl.owlCarousel({
-        items: 4,
+        items: 5,
         margin: 10,
         loop: true,
-        nav: true,
         dots: false,
+        nav: true,
         navText: [
             "<i aria-hidden='true' class='fas fa-chevron-left'></i>",
             "<i aria-hidden='true' class='fas fa-chevron-right'></i>",
         ],
         responsive: {
+            768: {
+                items: 3,
+            },
             480: {
                 items: 2,
             },
-            768: {
-                items: 3,
-            }
         }
     });
 }); 
